4425e9a30241055153c14b4bbdd443b46f80e70b
Dillon Johnson <dillonj323@gmail.com>
2023-11-09 18:12:57 +1300

    add a new time function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function startTime() {
+async function startTime() {
   const today = new Date()
   let h = today.getHours()
   let m = today.getMinutes()
@@ -9,9 +9,38 @@ function startTime() {
   setTimeout(startTime, 1000)
 }
 
+async function timeAhead() {
+  // clearTimeout(timeAheadTimeout)
+  const time = new Date()
+  let h = time.getHours() + 8
+  let m = time.getMinutes()
+  let s = time.getSeconds()
+
+  h = checkHour(h)
+  m = checkTime(m)
+  s = checkTime(s)
+
+  document.getElementById('timeAhead').innerHTML = h + ':' + m + ':' + s
+  timeAheadTimeout = setTimeout(timeAhead, 1000)
+}
+
+function clearTime() {
+  document.getElementById('timeAhead').innerHTML = ''
+  clearTimeout(timeAheadTimeout)
+}
+
+function checkHour(h) {
+  if (h > 24) {
+    h = h - 24
+  }
+  return h
+}
+
 function checkTime(i) {
   if (i < 10) {
     i = '0' + i
   }
   return i
 }
+
+startTime()
